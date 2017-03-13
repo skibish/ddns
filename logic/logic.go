e5a4f538413391d9d7be79459a62f3a98c6fc9c9
Sergey Kibish <sergey.kibish@gmail.com>
2017-03-13 23:08:33 +0200

    Remove unused package `logic`

--- logic/logic.go
+++
@@ -1,7 +0,0 @@
-package logic
-
-// PrepareEnivonment prepares working environment
-func PrepareEnivonment() {}
-
-// CheckAndUpdateIP checks, if IP has been changed. If yes, records will be updated
-func CheckAndUpdateIP() {}
