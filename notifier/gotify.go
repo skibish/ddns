skibish/ddns

commit 4d0ac3420389dffda70e25892465805bc9163fc7

BREAKING CHANGE: DDNS v4
- new configuration file: `ddns.yml`
- new configuraiton file format
- new flags; some flags removed and made as part of configuration file
- built with goreleaser
- updated `README.md`
- better logging

notifier/gotify.go
@@ -1,102 +1,81 @@
 package notifier
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
-	"os"
 	"strings"
 
+	"github.com/mitchellh/mapstructure"
 	"github.com/sirupsen/logrus"
+	"github.com/skibish/ddns/misc"
 )
 
-// GotifyConfig is a structure for Gotify notifications configuration
-type GotifyConfig struct {
-	AppURL   string `json:"app_url"`
-	AppToken string `json:"app_token"`
-	send     func(string) error
-	errorW   io.Writer
+// gotifyHook is a structure for Gotify notifications configuration
+type gotifyHook struct {
+	AppURL   string `mapstructure:"app_url"`
+	AppToken string `mapstructure:"app_token"`
+	Title    string
+	c        *http.Client
 }
 
-func initGotifyNotifier(cfg interface{}) (*GotifyConfig, error) {
-	// because in YAML we can have keys of complex type, they are usually of type
-	// map[interface{}]interface{}. In case for this hook we are interesting to convert
-	// it to map[string]string.
-	originalCfg, ok := cfg.(map[interface{}]interface{})
-	if !ok {
-		return nil, errors.New("not converted passed configuration")
-	}
-	m2 := make(map[string]interface{})
-
-	for key, value := range originalCfg {
-		switch key := key.(type) {
-		case string:
-			m2[key] = value
-		default:
-			return nil, errors.New("all keys should be strings in YAML")
-		}
+func newGotifyhook(cfg interface{}) (*gotifyHook, error) {
+	var hook gotifyHook
+	if err := mapstructure.Decode(cfg, &hook); err != nil {
+		return nil, err
 	}
 
-	// here we mashalling-unmarshalling to fill structure with correct values
-	b, errMarshal := json.Marshal(m2)
-	if errMarshal != nil {
-		return nil, errMarshal
+	if hook.Title == "" {
+		hook.Title = "DDNS"
 	}
 
-	var g GotifyConfig
-	errUnm := json.Unmarshal(b, &g)
-	if errUnm != nil {
-		return nil, errUnm
+	if !isValidURL(hook.AppURL) {
+		return nil, errors.New("app_url is not a valid url")
 	}
 
-	if !isValidURL(g.AppURL) {
-		return nil, errors.New("app_url is not a valid URL")
+	hook.AppURL = strings.TrimSuffix(hook.AppURL, "/")
+	hook.c = &http.Client{}
+
+	return &hook, nil
+}
+
+func (hook *gotifyHook) send(msg string) error {
+	form := &url.Values{}
+	form.Add("title", hook.Title)
+	form.Add("message", msg)
+
+	url := fmt.Sprintf("%s/message?token=%s", hook.AppURL, hook.AppToken)
+	req, err := http.NewRequest(http.MethodGet, url, strings.NewReader(form.Encode()))
+	if err != nil {
+		return fmt.Errorf("failed to create a request: %w", err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	res, err := hook.c.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to do a request: %w", err)
+	}
+	defer res.Body.Close()
+
+	if !misc.Success(res.StatusCode) {
+		return fmt.Errorf("status code is not in a success range: %w", err)
 	}
 
-	g.AppURL = strings.TrimSuffix(g.AppURL, "/")
-
-	g.errorW = os.Stdout
-	// function that send notification to gotify
-	g.send = func(msg string) error {
-		u := fmt.Sprintf("%s/message?token=%s", g.AppURL, g.AppToken)
-		data := url.Values{
-			"title":   {"DDNS"},
-			"message": {msg},
-		}
-
-		_, err := http.PostForm(u, data)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	}
-
-	return &g, nil
+	return nil
 }
 
 // Fire fires hook
-func (g *GotifyConfig) Fire(entry *logrus.Entry) error {
-	// ignoring recording of events on DEBUG
-	if entry.Level == logrus.DebugLevel {
-		return nil
-	}
-
-	errSend := g.send(entry.Message)
-	if errSend != nil {
-		fmt.Fprintf(g.errorW, "unable to send message to app: %v\n", errSend)
-		return errSend
+func (hook *gotifyHook) Fire(entry *logrus.Entry) error {
+	if err := hook.send(entry.Message); err != nil {
+		return fmt.Errorf("failed to send a message to app %w", err)
 	}
 
 	return nil
 }
 
 // Levels return array of levels
-func (g *GotifyConfig) Levels() []logrus.Level {
-	return logrus.AllLevels
+func (hook *gotifyHook) Levels() []logrus.Level {
+	return AllowedLevels()
 }
 
 // Source: https://golangcode.com/how-to-check-if-a-string-is-a-url/

notifier/gotify_test.go
@@ -1,90 +1,80 @@
 package notifier
 
 import (
-	"bufio"
-	"bytes"
-	"errors"
+	"net/http"
 	"testing"
 
+	"github.com/matryer/is"
 	"github.com/sirupsen/logrus"
 )
 
-func TestGotifyInit(t *testing.T) {
-	_, errConv := initGotifyNotifier("cfg")
-	if errConv == nil {
-		t.Error("Should be conversion error, but got nothing")
-		return
+func TestGotifyHookNew(t *testing.T) {
+	is := is.New(t)
+
+	if _, err := newGotifyhook("cfg"); err == nil {
+		is.Fail() // should be error, but got nothing
 	}
 
 	m := make(map[interface{}]interface{})
 	m["app_url"] = 123
-	_, errValue := initGotifyNotifier(m)
-	if errValue == nil {
-		t.Errorf("Should be error, because value is not a string")
-		return
-	}
-
-	m = make(map[interface{}]interface{})
-	m[123] = "app_url"
-	_, errKey := initGotifyNotifier(m)
-	if errKey == nil {
-		t.Errorf("Should be error, because key is not a string")
+	if _, err := newGotifyhook(m); err == nil {
+		is.Fail() // should fail because not a valid string
 		return
 	}
 
 	m = make(map[interface{}]interface{})
 	m["app_url"] = "https://gotify.example.com/"
-	g, errUnexpected := initGotifyNotifier(m)
-	if errUnexpected != nil {
-		t.Error(errUnexpected)
-		return
-	}
+	hook, err := newGotifyhook(m)
+	is.NoErr(err)
+	is.Equal(hook.AppURL, "https://gotify.example.com")
+	is.Equal(hook.Title, "DDNS")
 
-	if g.AppURL != "https://gotify.example.com" {
-		t.Errorf("app_url should be %q, but got %q", "https://gotify.example.com", g.AppURL)
-		return
+	m["app_url"] = "something bad"
+	if _, err := newGotifyhook(m); err == nil {
+		is.Fail() // should fail because incorrect url
 	}
 
 }
 
 func TestGotifyFire(t *testing.T) {
-
-	var b bytes.Buffer
-	writer := bufio.NewWriter(&b)
-
-	g := GotifyConfig{
-		AppURL:   "https://example.com/",
-		AppToken: "1234",
-		errorW:   writer,
+	tcases := []struct {
+		tname      string
+		statusCode int
+		isErr      bool
+	}{
+		{"ok", http.StatusOK, false},
+		{"fail do", http.StatusTeapot, true},
+		{"fail status", http.StatusTeapot, true},
 	}
 
-	g.send = func(msg string) error {
-		return nil
-	}
+	for _, tc := range tcases {
+		t.Run(tc.tname, func(t *testing.T) {
+			is := is.New(t)
 
-	entry := logrus.Entry{Message: "awesome message", Level: logrus.InfoLevel}
-	errFire := g.Fire(&entry)
-	if errFire != nil {
-		t.Errorf("handled an error on Gotify request: %q", errFire.Error())
-		return
-	}
+			url, close := httpHelper(t, tc.tname, nil, tc.statusCode)
+			defer close()
 
-	// test, got some error from Gotify
-	g.send = func(string) error {
-		return errors.New("something went wrong")
-	}
+			hook, err := newGotifyhook(map[string]interface{}{
+				"app_url": url,
+				"token":   "1234",
+				"title":   "DDNS",
+			})
+			is.NoErr(err)
 
-	errFire = g.Fire(&entry)
-	if errFire == nil {
-		t.Error("Should be error, but it's OK")
-		return
-	}
+			if tc.tname == "fail do" {
+				hook.AppURL = " "
+			}
 
-	// test, DEBUG is ignored
-	entry = logrus.Entry{Message: "ignored", Level: logrus.DebugLevel}
-	shouldBeNil := g.Fire(&entry)
-	if shouldBeNil != nil {
-		t.Errorf("Expected nil, on DEBUG, but got %v", shouldBeNil)
-		return
+			entry := logrus.Entry{Message: tc.tname, Level: logrus.InfoLevel}
+			err = hook.Fire(&entry)
+
+			if tc.isErr {
+				if err == nil {
+					t.Fail() // should be error
+				}
+				return
+			}
+			is.NoErr(err)
+		})
 	}
 }

notifier/notifier_test.go
@@ -1,0 +1,82 @@
+package notifier
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/matryer/is"
+)
+
+func httpHelper(t *testing.T, response string, headers map[string]string, statusCode int) (string, func()) {
+	is := is.New(t)
+	is.Helper()
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for k, v := range headers {
+			w.Header().Add(k, v)
+		}
+		w.WriteHeader(statusCode)
+		w.Write([]byte(response))
+	}))
+
+	return server.URL, server.Close
+}
+
+func TestGetHook(t *testing.T) {
+	tcases := []struct {
+		tname  string
+		config interface{}
+		isErr  bool
+	}{
+		{
+			tname: "ok smtp",
+			config: map[string]string{
+				"type": "smtp",
+				"from": "a@a.io",
+				"to":   "b@b.io",
+			},
+		},
+		{
+			tname: "ok telegram",
+			config: map[string]string{
+				"type":    "telegram",
+				"chat_id": "someid",
+				"token":   "1234",
+			},
+		},
+		{
+			tname: "ok gotify",
+			config: map[string]string{
+				"type":    "gotify",
+				"app_url": "https://gotify.example.com/",
+			},
+		},
+		{
+			tname:  "fail decode type",
+			config: map[string]interface{}{"type": 1234},
+			isErr:  true,
+		},
+		{
+			tname:  "fail do not exist",
+			config: map[string]string{"type": "zzz"},
+			isErr:  true,
+		},
+	}
+
+	for _, tc := range tcases {
+		t.Run(tc.tname, func(t *testing.T) {
+			is := is.New(t)
+
+			_, err := GetHook(tc.config)
+			if tc.isErr {
+				if err == nil {
+					is.Fail() // should be error
+				}
+				return
+			}
+
+			is.NoErr(err)
+		})
+	}
+}

notifier/telegram_test.go
@@ -1,90 +1,70 @@
 package notifier
 
 import (
-	"bufio"
-	"bytes"
-	"errors"
+	"net/http"
 	"testing"
 
+	"github.com/matryer/is"
 	"github.com/sirupsen/logrus"
 )
 
-func TestTelegramInit(t *testing.T) {
-	_, errConv := initTelegramNotifier("cfg")
-	if errConv == nil {
-		t.Error("Should be conversion error, but got nothing")
-		return
+func TestTelegramHookNew(t *testing.T) {
+	is := is.New(t)
+
+	if _, err := newTelegramHook("cfg"); err == nil {
+		is.Fail() // should be error, but got nothing
 	}
 
 	m := make(map[interface{}]interface{})
-	m["chat_id"] = 123
-	_, errValue := initTelegramNotifier(m)
-	if errValue == nil {
-		t.Errorf("Should be error, because value is not a string")
-		return
-	}
-
-	m = make(map[interface{}]interface{})
-	m[123] = "123"
-	_, errKey := initTelegramNotifier(m)
-	if errKey == nil {
-		t.Errorf("Should be error, because key is not a string")
-		return
-	}
-
-	m = make(map[interface{}]interface{})
 	m["chat_id"] = "123"
-	tg, errUnexpected := initTelegramNotifier(m)
-	if errUnexpected != nil {
-		t.Error(errUnexpected)
-		return
-	}
+	m["token"] = "tokenv"
 
-	if tg.ChatID != "123" {
-		t.Errorf("User should be %q, but got %q", "123", tg.ChatID)
-		return
-	}
+	hook, err := newTelegramHook(m)
 
+	is.NoErr(err)
+	is.Equal(hook.ChatID, "123")
+	is.Equal(hook.Token, "tokenv")
 }
 
-func TestTelegramFire(t *testing.T) {
-
-	var b bytes.Buffer
-	writer := bufio.NewWriter(&b)
-
-	tg := TelegramConfig{
-		Token:  "aaaa",
-		ChatID: "1234",
-		errorW: writer,
+func TestTelegramHookFire(t *testing.T) {
+	tcases := []struct {
+		tname      string
+		statusCode int
+		isErr      bool
+	}{
+		{"ok", http.StatusOK, false},
+		{"fail do", http.StatusTeapot, true},
+		{"fail status", http.StatusTeapot, true},
 	}
 
-	tg.send = func(msg string) error {
-		return nil
-	}
+	for _, tc := range tcases {
+		t.Run(tc.tname, func(t *testing.T) {
+			is := is.New(t)
 
-	entry := logrus.Entry{Message: "awesome message", Level: logrus.InfoLevel}
-	errFire := tg.Fire(&entry)
-	if errFire != nil {
-		t.Errorf("handled an error on Telegram request: %q", errFire.Error())
-		return
-	}
+			url, close := httpHelper(t, tc.tname, nil, tc.statusCode)
+			defer close()
 
-	// test, got some error from Telegram
-	tg.send = func(string) error {
-		return errors.New("something went wrong")
-	}
+			hook, err := newTelegramHook(map[string]interface{}{
+				"chat_id": "someid",
+				"token":   "1234",
+			})
+			is.NoErr(err)
 
-	errFire = tg.Fire(&entry)
-	if errFire == nil {
-		t.Error("Should be error, but it's OK")
-		return
-	}
+			hook.host = url
+			if tc.tname == "fail do" {
+				hook.host = " "
+			}
 
-	// test, DEBUG is ignored
-	entry = logrus.Entry{Message: "ignored", Level: logrus.DebugLevel}
-	shouldBeNil := tg.Fire(&entry)
-	if shouldBeNil != nil {
-		t.Errorf("Expected nil, on DEBUG, but got %v", shouldBeNil)
-		return
+			entry := logrus.Entry{Message: tc.tname, Level: logrus.InfoLevel}
+			err = hook.Fire(&entry)
+
+			if tc.isErr {
+				if err == nil {
+					t.Fail() // should be error
+				}
+				return
+			}
+			is.NoErr(err)
+		})
 	}
 }

commit 4f19dd54673f40684c0aef9eaef2af86756e9c24

fix: gotify http method must be POST (#40)
Gotify expects messages to be delivered with `POST` method, not `GET` ([docs](https://gotify.net/docs/pushmsg)). Bug was introduced in https://github.com/skibish/ddns/commit/4d0ac3420389dffda70e25892465805bc9163fc7#diff-d1574fc19caffa382481ccc99b3b6509878e689a3f0a74e3e59f718d094e0a67L70-R58 (4 years ago) – typo error. This pr fixes #39

notifier/gotify.go
@@ -46,7 +46,7 @@ func (hook *gotifyHook) send(msg string) error {
 	form.Add("message", msg)
 
 	url := fmt.Sprintf("%s/message?token=%s", hook.AppURL, hook.AppToken)
-	req, err := http.NewRequest(http.MethodGet, url, strings.NewReader(form.Encode()))
+	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(form.Encode()))
 	if err != nil {
 		return fmt.Errorf("failed to create a request: %w", err)
 	}

notifier/gotify_test.go
@@ -51,7 +51,7 @@ func TestGotifyFire(t *testing.T) {
 		t.Run(tc.tname, func(t *testing.T) {
 			is := is.New(t)
 
-			url, close := httpHelper(t, tc.tname, nil, tc.statusCode)
+			url, close := httpHelper(t, tc.tname, http.MethodPost, nil, tc.statusCode)
 			defer close()
 
 			hook, err := newGotifyhook(map[string]interface{}{

notifier/notifier_test.go
@@ -8,11 +8,15 @@ import (
 	"github.com/matryer/is"
 )
 
-func httpHelper(t *testing.T, response string, headers map[string]string, statusCode int) (string, func()) {
+func httpHelper(t *testing.T, response string, requestMethod string, headers map[string]string, statusCode int) (string, func()) {
 	is := is.New(t)
 	is.Helper()
 
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		is.Helper()
+
+		is.Equal(r.Method, requestMethod)
+
 		for k, v := range headers {
 			w.Header().Add(k, v)
 		}

notifier/telegram_test.go
@@ -41,7 +41,7 @@ func TestTelegramHookFire(t *testing.T) {
 		t.Run(tc.tname, func(t *testing.T) {
 			is := is.New(t)
 
-			url, close := httpHelper(t, tc.tname, nil, tc.statusCode)
+			url, close := httpHelper(t, tc.tname, http.MethodGet, nil, tc.statusCode)
 			defer close()
 
 			hook, err := newTelegramHook(map[string]interface{}{